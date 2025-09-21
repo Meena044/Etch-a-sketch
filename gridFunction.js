b225a999a1439ad1313ad7453ad602aa6b75a39c
Meena044 <meena290470@gmail.com>
2025-09-21 20:13:34 +0530

    added progressing darken color feature

--- gridFunction.js
+++ gridFunction.js
@@ -1,9 +1,7 @@
-
-let input= document.querySelector('#size');
 const container = document.querySelector(".container");
 defaultGrid(16);
 
-
+let input= document.querySelector('#size');
 input.addEventListener("click", (e)=>{
     
     let value=prompt("enter size");
@@ -17,12 +15,26 @@ input.addEventListener("click", (e)=>{
 })
 
 const reset = document.querySelector("#reset");
-reset.addEventListener("click", ()=>{
+reset.addEventListener("click", (e)=>{
     container.innerHTML = "";
     defaultGrid(16);
 })
 
-// default grid function 16x16 grid
+
+const buttons = document.querySelectorAll(".buttons button");
+buttons.forEach((btn) => {
+    btn.addEventListener("mousedown", (e)=>{
+        e.target.style.backgroundColor = "rgba(213, 184, 228, 1)";
+    })
+});
+
+buttons.forEach((btnn)=>{
+    btnn.addEventListener("mouseout", (e)=>{
+    e.target.style.backgroundColor = "rgb(95, 8, 136)";
+    });
+});
+
+// grid function to create square divs dynamically
 function defaultGrid(size){
     for(let i=0; i< size; i++){
         for(let j=0; j< size ; j++){
@@ -33,10 +45,10 @@ function defaultGrid(size){
             grid.dataset.colorCounter = 0; //in HTML it will look like data-colorCounter
 
             grid.addEventListener("mouseover", (e)=> {
-                let shade = parseInt(grid.dataset.colorCounter);
+                let shade = parseInt(grid.dataset.colorCounter); //dataser stores value as string
                 if(shade<10){
                     shade += 1
-                    e.target.style.backgroundColor =`rgba(69, 10, 117, ${shade * 0.1} )` ;
+                    e.target.style.backgroundColor = `rgba(252, 8, 40, ${shade * 0.1} )` ;
                     grid.dataset.colorCounter = shade;
                 }
             })
